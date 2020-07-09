79e3a1702bb787a899229567dbf696b6ded44a08
yossyi <39930174+nkue-yst@users.noreply.github.com>
2020-07-09 15:54:42 +0900

    Updates

---
+++ precache-manifest.641b9553c3aa39be79afe7f062659dae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ebb13d832b5009d3a7b66919f05f9b3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2d315ff5473470eb9836",
+    "url": "/static/css/main.a8698a51.chunk.css"
+  },
+  {
+    "revision": "25d73f7fb44831c22c66",
+    "url": "/static/js/2.c4a5c059.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.c4a5c059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d315ff5473470eb9836",
+    "url": "/static/js/main.7b67530b.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  }
+]);
