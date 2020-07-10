b6a1408b412b7752b62f5e6576d0f91091c0dc09
yossyi <39930174+nkue-yst@users.noreply.github.com>
2020-07-10 12:24:41 +0900

    Fix menu bar

--- src/MenuBar.js
+++ src/MenuBar.js
@@ -3,10 +3,12 @@ import PropTypes from 'prop-types';
 import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Box from '@material-ui/core/Box';
+import Container from '@material-ui/core/Container';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typogarphy from '@material-ui/core/Typography';
+import useScrollTrigger from '@material-ui/core/useScrollTrigger';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -22,6 +24,22 @@ function allayProps(index) {
     id: `tab-${index}`,
     'aria-controls': `tabpanel-${index}`,
   };
+}
+
+function ElevationScroll(prop) {
+  const { children } = prop;
+
+  const trigger = useScrollTrigger({
+    disableHysteresis: true,
+    threshold: 0,
+  });
+
+  return React.cloneElement(children, {
+    elevation: trigger ? 4 : 0,
+  });
+}
+ElevationScroll.propTypes = {
+  children: PropTypes.element.isRequired,
 }
 
 function TabPanel(props) {
@@ -43,14 +61,13 @@ function TabPanel(props) {
     </div>
   );
 }
-
 TabPanel.propTypes = {
   children: PropTypes.node,
   index: PropTypes.any.isRequired,
   value: PropTypes.any.isRequired,
 };
 
-export default function MenuBar() {
+export default function MenuBar(props) {
   const classes = useStyles();
   const [value, setValue] = React.useState(0);
   const handleChange = (event, newValue) => {
@@ -59,22 +76,38 @@ export default function MenuBar() {
 
   return (
     <div className={classes.root}>
-      <AppBar position="static">
-        <Toolbar>
-          <Typogarphy variant="h6" className={classes.title}>
-            Nakaue's Portfolio
-          </Typogarphy>
-        </Toolbar>
-        <Tabs value={value} onChange={handleChange} aria-label="tabs">
-          <Tab label="About" {...allayProps(0)} />
-          <Tab label="Work"  {...allayProps(1)} />
-        </Tabs>
-      </AppBar>
+      <ElevationScroll {...props}>
+        <AppBar>
+          <Toolbar>
+            <Typogarphy variant="h5" className={classes.title}>
+              Nakaue's Portfolio
+            </Typogarphy>
+            <Tabs value={value} onChange={handleChange} aria-label="tabs">
+              <Tab label="About" {...allayProps(0)} />
+              <Tab label="Work"  {...allayProps(1)} />
+            </Tabs>
+          </Toolbar>
+        </AppBar>
+      </ElevationScroll>
       <TabPanel value={value} index={0}>
-        About
+        <Container>
+          <Box my={2}>
+            {[...new Array(1000)].map(() => 
+              `About`,
+              ).join('\n')
+            }
+          </Box>
+        </Container>
       </TabPanel>
       <TabPanel value={value} index={1}>
-        Work
+        <Container>
+          <Box my={2}>
+            {[...new Array(1000)].map(() => 
+              `Work`,
+              ).join('\n')
+            }
+          </Box>
+        </Container>
       </TabPanel>
     </div>
   );
