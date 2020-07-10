88967dc6f8be272db9ec82c3d7d5948b33452e76
yossyi <39930174+nkue-yst@users.noreply.github.com>
2020-07-10 11:28:58 +0900

    Add tabs to menu bar

--- src/MenuBar.js
+++ src/MenuBar.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
-import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
+import Box from '@material-ui/core/Box';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typogarphy from '@material-ui/core/Typography';
 
@@ -11,32 +12,70 @@ const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
   },
-  menuButton: {
-    marginButton: {
-      marginRight: theme.spacing(2),
-    },
-    title: {
-      flexGrow: 1,
-    },
+  title: {
+    flexGrow: 1,
   },
 }));
 
+function allayProps(index) {
+  return {
+    id: `tab-${index}`,
+    'aria-controls': `tabpanel-${index}`,
+  };
+}
+
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`tabpanel-${index}`}
+      aria-labelledby={`tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box p={3}>
+          <Typogarphy>{children}</Typogarphy>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.any.isRequired,
+  value: PropTypes.any.isRequired,
+};
+
 export default function MenuBar() {
   const classes = useStyles();
+  const [value, setValue] = React.useState(0);
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
 
   return (
     <div className={classes.root}>
       <AppBar position="static">
         <Toolbar>
-          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-            <MenuIcon />
-          </IconButton>
           <Typogarphy variant="h6" className={classes.title}>
             Nakaue's Portfolio
           </Typogarphy>
-          <Button color="inherit"></Button>
         </Toolbar>
+        <Tabs value={value} onChange={handleChange} aria-label="tabs">
+          <Tab label="About" {...allayProps(0)} />
+          <Tab label="Work"  {...allayProps(1)} />
+        </Tabs>
       </AppBar>
+      <TabPanel value={value} index={0}>
+        About
+      </TabPanel>
+      <TabPanel value={value} index={1}>
+        Work
+      </TabPanel>
     </div>
   );
 }
